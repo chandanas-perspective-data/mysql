60cdf5e322f4602b644712b7a26053ce569984fe
ChandanaSree <99187261+chandanas-perspective-data@users.noreply.github.com>
2022-02-18 17:14:04 +0530

    Add files via upload

---
+++ creating a table.sql
@@ -0,0 +1,13 @@
+create database database1;
+use database1;
+create table table2(Name varchar(20),id int ,dept varchar(20), Marks int );
+select * from table2;
+insert into table2 values( 'chandanasree',1,'telugu',35);
+insert into table2 values('vamshi',2,'hindi',45);
+insert into table2 values('siddhu',3 ,'english',50);
+insert into table2 values('shirisha',4,'social',38);
+insert into table2 values('padmaja', 5,'science',36);
+insert into table2 values('susmitha',6,'physics',42);
+insert into table2 values('kalyani',7,'chemistry',37);
+insert into table2 values('shruthi',8,'zoology',54);
+select*from table2;
